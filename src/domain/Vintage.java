b713c5126aabd8a2ed5aee026caf49b268b1986b
murcia0421 <112002572+murcia0421@users.noreply.github.com>
2023-11-18 20:34:11 -0500

    Vintage.java

--- src/domain/Vintage.java
+++ src/domain/Vintage.java
@@ -7,8 +7,21 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.LinkedList;
 
+
+/**
+ * The Vintage class represents a game board for the "Vintage" game.
+ * It includes methods for playing moves, scoring, saving and loading game states,
+ * and initializing the game board with gems and players.
+ *
+ * The game board is a square grid, and players take turns swapping adjacent gems
+ * to create matches of three or more in a row or column.
+ *
+ * @author Mateo Forero, Juan Murcia
+ * @version 1.0
+ */
 public class Vintage implements Serializable{
-	
+
+       // Instance variables
 	public final int size;
 	private int[][] game;
 	private boolean[][] visited;
@@ -20,6 +33,11 @@ public class Vintage implements Serializable{
 	public int player2Score;
 	private boolean turn = true;
 	
+	/**
+        * Constructs a Vintage object with the specified size.
+        *
+        * @param n The size of the square game board.
+        */
 	public Vintage(int n) {
 		size = n;
 		init(n);
@@ -27,6 +45,15 @@ public class Vintage implements Serializable{
 		this.player2 = "Player2";
 		
 	}
+
+
+	/**
+     	* Constructs a Vintage object with the specified size and player names.
+     	*
+     	* @param n        The size of the square game board.
+     	* @param player1  The name of player 1.
+    	* @param player2  The name of player 2.
+     	*/
 	public Vintage(int n,String player1, String player2) {
 		size = n;
 		init(n);
@@ -36,7 +63,13 @@ public class Vintage implements Serializable{
 		this.player2 = player2;
 	}
 
-	
+	/**
+	* Performs a move by swapping gems at the specified locations.
+	*
+	* @param first  The coordinates of the first gem.
+	* @param second The coordinates of the second gem.
+	* @return true if the move is valid, false otherwise.
+	*/
 	public boolean play(int[]first,int[]second) {
 		boolean valid=false;
 		double dis = Math.sqrt(Math.pow(first[0]-second[0],2)+Math.pow(first[1]-second[1],2));
@@ -52,7 +85,14 @@ public class Vintage implements Serializable{
 		}
 		return valid;
 	}
-	
+
+
+	/**
+     	* Performs actions related to a gem at the specified location.
+     	*
+     	* @param row    The row of the gem.
+     	* @param column The column of the gem.
+     	*/
 	public void play(int row, int column) {
 		int score = 0;
 		int[][] move = diagonalMove(row,column);
@@ -65,6 +105,13 @@ public class Vintage implements Serializable{
 		score += setScoreMoves(move);
 		sumScore(score);
 	}
+
+	/**
+	* Updates the game state and calculates the score based on the given moves.
+	*
+	* @param moves A 2D array representing the coordinates of the matched gems.
+	* @return The score achieved by the moves.
+	*/
 	private int setScoreMoves(int [][] moves) {
 		int score=0;
 		if (moves != null){
@@ -86,6 +133,15 @@ public class Vintage implements Serializable{
 		}
 		return score;
 	}
+
+
+	/**
+	* Checks for matched gems in the diagonal direction starting from the specified cell.
+	*
+	* @param row    The row of the cell.
+	* @param column The column of the cell.
+	* @return A 2D array containing the coordinates of the matched gems if found; otherwise, returns null.
+	*/
 	private int[][] diagonalMove(int row, int column) {
 		int up = 0;
 		while (verify(row-up-1,column-up-1) && game[row-up-1][column-up-1] == game[row][column] && up < 3) 
@@ -103,7 +159,15 @@ public class Vintage implements Serializable{
 		}
 		return res;
 	}
-	
+
+
+	/**
+	* Checks for matched gems in the reverse diagonal direction starting from the specified cell.
+	*
+	* @param row    The row of the cell.
+	* @param column The column of the cell.
+	* @return A 2D array containing the coordinates of the matched gems if found; otherwise, returns null.
+	*/
 	private int[][] diagonalIMove(int row, int column) {
 		int up = 0;
 		while (verify(row-up-1,column+up+1) && game[row-up-1][column+up+1] == game[row][column] && up < 3) 
@@ -121,7 +185,15 @@ public class Vintage implements Serializable{
 		}
 		return res;
 	}
-	
+
+
+	/**
+	* Checks for matched gems in the horizontal direction starting from the specified cell.
+	*
+	* @param row    The row of the cell.
+	* @param column The column of the cell.
+	* @return A 2D array containing the coordinates of the matched gems if found; otherwise, returns null.
+	*/
 	private int[][] horizontalMove(int row, int column) {
 		int lf = 0;
 		while (verify(row,column-lf-1) && game[row][column-lf-1] == game[row][column] && lf < 3) 
@@ -139,7 +211,14 @@ public class Vintage implements Serializable{
 		}
 		return res;
 	}
-	
+
+	/**
+ 	* Checks for matched gems in the vertical direction starting from the specified cell.
+ 	*
+ 	* @param row    The row of the cell.
+	* @param column The column of the cell.
+ 	* @return A 2D array containing the coordinates of the matched gems if found; otherwise, returns null.
+ 	*/
 	private int[][] verticalMove(int row, int column) {
 		int up = 0;
 		while (verify(row-up-1,column) && game[row-up-1][column] == game[row][column] && up < 3) 
@@ -157,7 +236,13 @@ public class Vintage implements Serializable{
 		}
 		return res;
 	}
-	
+
+	/**
+	* Performs the falling animation for gems in the specified column after some gems have been removed.
+	*
+	* @param r The row of the cell.
+	* @param c The column of the cell.
+	*/
 	private void makeFall(int r,int c) {
 		ArrayList<Integer> arr1 = new ArrayList<Integer>();	
 		for(int i = r ; i >= 0 ; i--) {
@@ -189,6 +274,12 @@ public class Vintage implements Serializable{
 	}
 
 
+
+	/**
+	* Saves the current game state to a specified file.
+	*
+	* @param nombreArchivo The name of the file to save the game state.
+	*/
 	public void guardarPartida(String nombreArchivo) {
 	    ObjectOutputStream salida = null;
 	    try {
@@ -208,7 +299,14 @@ public class Vintage implements Serializable{
 	        }
 	    }
 	}
-	
+
+
+	/**
+ 	* Loads a game state from a specified file and returns a Vintage object.
+ 	*
+ 	* @param nombreArchivo The name of the file to load the game state.
+ 	* @return A Vintage object representing the loaded game state.
+	*/
 	public static Vintage cargarPartida(String nombreArchivo) {
 		Vintage partidaCargada = null;
 	    try {
@@ -220,18 +318,38 @@ public class Vintage implements Serializable{
 	    } 
 	    return partidaCargada;
 	}
-	
+
+	/**
+	* Updates the score based on the provided value.
+	*
+	* @param s The score to be added.
+	*/
 	private void sumScore(int s){
 		if (turn)player1Score += s;
 		else player2Score += s;
 	}
-	
+
+
+	/**
+ 	* Checks whether the given coordinates are within the bounds of the game board.
+ 	*
+ 	* @param r The row coordinate.
+ 	* @param c The column coordinate.
+ 	* @return true if the coordinates are within bounds, otherwise false.
+ 	*/
 	private boolean verify(int r, int c) {
 		boolean flag = false;
 		if (0 <= r && r < size && 0 <= c && c < size) flag = true;
 		return flag;
 	}
 	
+	
+	
+	/**
+ 	* Initializes the game by creating the game board, setting initial values, and preparing the game state.
+	*
+ 	* @param n The size of the game board.
+ 	*/
 	private void init(int n) {
 		game = new int[n][n];
 		for(int i = 0;i < n;i++) {
@@ -252,6 +370,9 @@ public class Vintage implements Serializable{
 		player2Score = 0;
 	}
 	
+	/**
+ 	* Modifies the entire game board by assigning the best possible assignment for each cell.
+	*/
 	private void modifyBoard() {
 		for(int i = 0;i < size;i++) {
 			for(int j = 0;j < size; j++) {
@@ -259,6 +380,14 @@ public class Vintage implements Serializable{
 			}
 		}
 	}
+
+	/**
+ 	* Determines the best assignment for a given cell based on the surrounding gems.
+ 	*
+ 	* @param row    The row of the cell.
+ 	* @param column The column of the cell.
+	 * @return The best assignment for the specified cell.
+	 */
 	public int bestAssigment(int row, int column) {
 		int bestMove = 0;
 		boolean[] fail = new boolean[numGems];
